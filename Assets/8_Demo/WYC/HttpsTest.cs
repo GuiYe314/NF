47164927e382cdd7ed87536cdce0f678c431d3eb
wangyc <wangyc@xayy.tech>
2024-06-26 18:52:07 +0800

    语音链接获取测试，处理模型

--- Assets/8_Demo/WYC/HttpsTest.cs
+++ Assets/8_Demo/WYC/HttpsTest.cs
@@ -8,15 +8,22 @@ using Best.HTTP.Request.Upload;
 using Best.HTTP.Request.Authenticators;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Unity.VisualScripting.Antlr3.Runtime;
+
 
 
 public class HttpsTest : MonoBehaviour
 {
 
+
+
+    public OperatingEnvironment operatingEnvironment;
+
+
     HTTPRequest re = null;
 
     string token = string.Empty;
+
+    public int index = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -36,15 +43,48 @@ public class HttpsTest : MonoBehaviour
     {
         if (GUILayout.Button("查询所有景点"))
         {
-            HTTPRequest request = HTTPRequest.CreateGet("http://cultural-tourism-dev.xayy.tech/api/cultural-tourism/v1.0/app/scenic/selectAllScenic",
-                                        Callback);
-            //request.UploadSettings.UploadStream = new MultipartFormDataStream();
-            request.Authenticator = new BearerTokenAuthenticator(token);
 
-            request.AddHeader("Authorization",token);
+            //HTTPRequest request = HTTPRequest.CreateGet($"http://cultural-tourism-{operatingEnvironment.ToString()}.xayy.tech/api/cultural-tourism/v1.0/app/scenicGuide/getAllScenicInfo/1788842419551207424", Callback);
+            HTTPRequest request = HTTPRequest.CreatePost($"https://cultural-tourism.xianyanyu.tech/api/cultural-tourism/v1.0/app/scenic/appPage", Callback);
+
+            request.AddHeader("Jwt-Token", token);
+
+            CS cS = new CS();
+            cS.isEnable = "1";
+            cS.pageNum = index.ToString();
+            cS.pageSize = "10";
+
+
+
+
+            request.UploadSettings.UploadStream = new JSonDataStream<CS>(cS);
+
             request.Send();
-
         }
+
+
+        if (GUILayout.Button("获取景点下的讲解包"))
+        {
+
+            //HTTPRequest request = HTTPRequest.CreatePost($"http://cultural-tourism-{operatingEnvironment.ToString()}.xayy.tech/api/cultural-tourism/v1.0/app/explanation/page", Callback);
+            HTTPRequest request = HTTPRequest.CreatePost($"https://cultural-tourism.xianyanyu.tech/api/cultural-tourism/v1.0/app/explanation/page", Callback);
+
+            request.AddHeader("Jwt-Token", token);
+
+            YYB yYB = new YYB();
+            yYB.pageNum = "0";
+            yYB.approveType = "1";
+            yYB.pageSize = "10";
+            yYB.roleId = "0";
+            yYB.scenicId = "1792441056474693632";
+
+            request.UploadSettings.UploadStream = new JSonDataStream<YYB>(yYB);
+
+            request.Send();
+        }
+
+
+
 
     }
 
@@ -53,13 +93,15 @@ public class HttpsTest : MonoBehaviour
     {
 
         // 1. Create request with a callback
-        HTTPRequest request = HTTPRequest.CreatePost("http://cultural-tourism-dev.xayy.tech/users/user/auth/form",
-                                             DLCallback);
+        //HTTPRequest request = HTTPRequest.CreatePost($"http://cultural-tourism-{operatingEnvironment.ToString()}.xayy.tech/users/user/auth/form",DLCallback);
+        HTTPRequest request = HTTPRequest.CreatePost($"https://cultural-tourism.xianyanyu.tech/users/user/auth/form",DLCallback);
 
         // 2. Setup request parameters
         request.UploadSettings.UploadStream = new MultipartFormDataStream()
             .AddField("username", "admin")
-            .AddField("password", "123456");
+            .AddField("password", "Xayy@123.");
+
+
 
 
         // 3. Send request
@@ -78,7 +120,7 @@ public class HttpsTest : MonoBehaviour
             case HTTPRequestStates.Finished:
                 if (resp.IsSuccess)
                 {
-
+                    Debug.LogError(resp.DataAsText);
                     JObject jo = (JObject)JsonConvert.DeserializeObject(resp.DataAsText);
                     token = jo["data"].ToString();
 
@@ -130,5 +172,42 @@ public class HttpsTest : MonoBehaviour
 
 
     }
+
+
+
+
+    //运行环境
+    public enum OperatingEnvironment
+    {
+        dev=0,
+        test,
+        demo
+    }
+
+    public class YYB
+    {
+
+        public string pageNum;
+        public string approveType;
+        public string id;
+        public string pageSize;
+        public string roleId;
+        public string scenicId;
+
+
+    }
+
+    public class CS
+    {
+
+        public string isEnable;
+        public string pageNum;
+        public string pageSize;
+        public string scenicArea;
+
+
+
+    }
+
 
 }
